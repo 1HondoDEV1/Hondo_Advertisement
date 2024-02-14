f0fa338fe3717a1426be0abb5fe811c69bbd1263
1HondoDEV1 <160051151+1HondoDEV1@users.noreply.github.com>
2024-02-14 17:10:23 +0000

    Add files via upload

---
+++ Hondo_Advertisement/config.lua
@@ -0,0 +1,37 @@
+Config = {}
+Config.Framework = 'qbcore' -- esx or qbcore
+Config.AdIcon = 'store' --Icon Displayed In The Advertisement Notification Find More Here https://fontawesome.com/icons
+
+Config.CoolDownTime = 600000 --Example 10 minutes = 600000 milliseconds.Time before allowing again
+
+Config.ShortNotificationTime = 5000 -- How long the notification is displayed 
+Config.ShortNotificationPrice = 500 -- Price for the notification to show for price above
+
+Config.MediumNotificationTime = 10000 -- How long the notification is displayed
+Config.MediumNotificationPrice = 1000 -- Price for the notification to show for price above
+
+Config.LongNotificationTime = 15000 -- How long the notification is displayed
+Config.LongNotificationPrice = 1500 -- Price for the notification to show for price above
+
+Config.AllowedJobs = {
+    'cardealer',
+    'mechanic',
+    'beeker',
+    'cannabiscafe',
+    'cookies',
+    'burgershot',
+    'chickfila',
+    'dominos',
+    'dunkin',
+    'icebox',
+    'pizzathis',
+    'pearls',
+    'popeyes',
+    'taco',
+    'wingstop',
+    'aldentes',
+    'cluckinbell',
+    'bahamamamas',
+    'craftbar',
+    'realestate'
+}
